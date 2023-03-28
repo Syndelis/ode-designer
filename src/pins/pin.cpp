acc850a183930ce489a65d432c34ce1be9c5b48f
Brenno Lemos <brenno@syndel.is>
2023-03-28 15:09:43 -0300

    Revert "chore: on-purpose breaking of standards as a trigger test"
    This reverts commit a12abeacff2a7c3861a4e05d00534564c409e735.

--- src/pins/pin.cpp
+++ src/pins/pin.cpp
@@ -1,6 +1,6 @@
 #include "pin.hpp"
 
-#include <stdio.h>
+#include <cstdio>
 #include <imnodes.h>
 #include <iostream>
 #include <map>
@@ -10,10 +10,7 @@
 
 #include "../nodes/node.hpp"
 
-void Pin::unlink(ElementID linkId)
-
-
-{
+void Pin::unlink(ElementID linkId) {
     for (auto &[id, pin] : allPins)
         if (pin->linkedTo.count(linkId)) {
             Pin *otherPin     = pin->linkedTo.extract(linkId).mapped().target;
