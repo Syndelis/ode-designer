d12b6561fd92ae69e3d54dc4238d9dbbf9720e62
S-Pz <saviofrancisco150@hotmail.com>
2023-08-17 15:41:59 -0300

    add implot and make to run the library

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -7,6 +7,7 @@
 #include <imgui_internal.h>
 #include <imgui.h>
 #include <imnodes.h>
+#include <implot.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 #include <GLFW/glfw3.h>
@@ -26,14 +27,9 @@
 #include "../nodes/combinator.hpp"
 #include "../nodes/node.hpp"
 #include "../nodes/population.hpp"
-<<<<<<< Updated upstream
-=======
-#include "../nodes/variable.hpp"
-#include "../portable-file-dialogs/portable-file-dialogs.h"
-
->>>>>>> Stashed changes
 #include "../pins/pin.hpp"
-#include "../portable-file-dialogs/portable-file-dialogs.h"
+#include "portable-file-dialogs.h"
+
 
 static bool isContextMenuOpen = false;
 
@@ -141,10 +137,7 @@ void test_save_file(){
                             { "Text Files (.txt .text)", "*.txt *.text" },
                             pfd::opt::force_overwrite);
 }
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 void process() {
 
     // Rendering -------------------------------------------
@@ -156,15 +149,7 @@ void process() {
 
         test_open_file();
     }
-
-<<<<<<< Updated upstream
-    if(ImGui::IsKeyPressed(ImGuiMod_Ctrl) | ImGui::IsKeyPressed(ImGuiKey_O)){
-
-        test_open_file();
-    }
-
-=======
->>>>>>> Stashed changes
+    
     if(ImGui::BeginMainMenuBar()){
         if(ImGui::BeginMenu("File")){
             MenuFile();
@@ -236,21 +221,7 @@ void process() {
 static void MenuFile(){
     
     if (ImGui::MenuItem("New")){}
-<<<<<<< Updated upstream
-    
-    if (ImGui::MenuItem("Open", "Ctrl+O")){
 
-       test_open_file();
-        
-    }
-    if (ImGui::MenuItem("Save", "Ctrl+S")) {
-        test_save_file();
-    }
-    if (ImGui::MenuItem("Save As..")) {
-        test_save_file();
-    }
-=======
->>>>>>> Stashed changes
     if (ImGui::MenuItem("Open", "Ctrl+O")){
 
        test_open_file();
@@ -338,6 +309,8 @@ int main() {
     new Combinator("abc");  // NOLINT(clang-diagnostic-writable-strings, clang-analyzer-cplusplus.NewDeleteLeaks)
     // clang-format on
 
+    ImPlot::CreateContext();
+
     // Main Loop -------------------------------------------
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
@@ -348,6 +321,7 @@ int main() {
         // Draw Start ---------------------------------------
 
         ImGui::ShowDemoWindow();
+        ImPlot::ShowDemoWindow();
         ImGuiID dock_id
             = ImGui::DockSpaceOverViewport(ImGui::GetMainViewport(), 0);
         ImGui::SetNextWindowDockID(dock_id, true);
